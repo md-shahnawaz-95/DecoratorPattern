920d5a79feafa5b4b4f9b5b171be63fdec25dc32
Md Shahnawaz <mdshahnawaz95@outlook.com>
2021-06-25 23:19:49 +0530

    changed the coffeeShope

--- src/com/decoratorPattern/coffeeShop.java
+++ src/com/decoratorPattern/coffeeShop.java
@@ -2,6 +2,6 @@ package com.decoratorPattern;
 
 public class coffeeShop {
     public static void main(String args[]){
-        System.out.println("This is observer pattern");
+        System.out.println("This is observer pattern.");
     }
 }
